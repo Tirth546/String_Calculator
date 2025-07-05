4802a0fc489ff75bfab32a284d635aa72a790569
Tirth546 <tirth546patel@gmail.com>
2025-07-05 14:36:49 +0530

    Add tests for single number and two-number inputs in add()

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,13 @@
-const add = require('./calculator');
+const add = require("./calculator");
 
-test('returns 0 for empty string', () => {
+test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
+});
+
+test("returns number itself if one number", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns sum of two numbers", () => {
+  expect(add("1,2")).toBe(3);
 });

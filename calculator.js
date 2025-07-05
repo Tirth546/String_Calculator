ea5374340d171887b304773832ec5b84e1a09557
Tirth546 <tirth546patel@gmail.com>
2025-07-05 15:03:52 +0530

    Update add() to support custom delimiter defined with //

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
 function add(numbers) {
   if (!numbers) return 0;
 
-  const replaced = numbers.replace(/\n/g, ",");
-  const parts = replaced.split(",");
-  const sum = parts.reduce((acc, val) => acc + parseInt(val), 0);
-  return sum;
+  let delimiter = /,|\n/;
+  let numberSection = numbers;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2)); // get custom delimiter
+    numberSection = parts[1];
+  }
+
+  const tokens = numberSection.split(delimiter);
+  const parsed = tokens.map((n) => parseInt(n));
+  return parsed.reduce((acc, val) => acc + val, 0);
 }
 
 module.exports = add;

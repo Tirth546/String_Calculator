2dfd5d26a508eefa97a144bd11ea1efb2e4f09ed
Tirth546 <tirth546patel@gmail.com>
2025-07-05 14:49:35 +0530

    Update add() to support multiple numbers and newline delimiters

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 function add(numbers) {
   if (!numbers) return 0;
 
-  const parts = numbers.split(",");
+  const replaced = numbers.replace(/\n/g, ",");
+  const parts = replaced.split(",");
   const sum = parts.reduce((acc, val) => acc + parseInt(val), 0);
   return sum;
 }
